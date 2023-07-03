90b4cdfba7aa45005e97c09dbd0615f071624a60
Park_Sang_Wook <124150788+parksangwook0207@users.noreply.github.com>
2023-07-03 19:54:01 +0900

    7.3ui강사님과 같이 작업중

--- Assets/0.Scripts/UI.cs
+++ Assets/0.Scripts/UI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UI : MonoBehaviour
@@ -8,8 +9,19 @@ public class UI : MonoBehaviour
     public static UI instance;
 
     [SerializeField] private TMP_Text text;
+    [SerializeField] private Image hpImage;
+    [SerializeField] private Image mpImage;
+
+    [SerializeField] private TMP_Text hptxt;
+    [SerializeField] private TMP_Text mptxt;
 
     int score;
+
+    int curHp = 100;
+    int maxHp = 100;
+
+    int curMp = 50;
+    int maxMp = 50;
 
     public int SetScore
     {
@@ -27,7 +39,51 @@ public class UI : MonoBehaviour
     private void Start()
     {
         instance = this;
-        
+
+        float value = curHp / maxHp;
+        hpImage.fillAmount = value;
+        hptxt.text = $"{(int)(value * 100f)}%";
+
+    }
+    private void Update()
+    {
+        // HP
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (Random.Range(0, 10) < 10)
+            {
+                curHp += Random.Range(5, 10);
+            }
+            else
+            {
+                curHp -= Random.Range(5, 10);
+            }
+            float value = curHp / maxHp;
+            hpImage.fillAmount = value;
+            hptxt.text = $"{(int)(value * maxHp)}%";
+        }
+        /*if (Input.GetKeyDown(KeyCode.H))
+        {
+            float value = curHp / maxHp;
+            hpImage.fillAmount = value;
+            hptxt.text = $"{(int)(value * 100f)}%";
+        }
+        */
+        // MP
+        /*if (Input.GetKeyDown(KeyCode.J))
+        {
+            mpImage.fillAmount += Time.deltaTime * 3;
+            mp += 1;
+            mptxt.text = $"{mp}/100";
+            
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            mpImage.fillAmount -= Time.deltaTime * 3;
+            mp -= 1;
+            mptxt.text = $"{mp}/100";
+        }
+        */
     }
 
 }
